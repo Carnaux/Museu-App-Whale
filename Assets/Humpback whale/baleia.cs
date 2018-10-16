b23791f9f70594ff953d53adcf69bad72f8ce57f
Carnaux <danielfeenandes@gmail.com>
2018-10-16 15:05:37 -0300

    Upload Files

---
+++ Assets/Humpback whale/baleia.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baleia : MonoBehaviour {
+
+    public AudioSource sound;
+  
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void play()
+    {
+        sound.Play();
+    }
+    
+}
---
+++ Assets/Humpback whale/baleiasom.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baleiasom : MonoBehaviour
+{
+
+    public AudioSource sound;
+    private Rect audiorect;
+
+    void Start()
+    {
+        audiorect = new Rect(20, 20, 100, 20);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(audiorect, "Play Audio");
+
+        if (audiorect.Contains(Event.current.mousePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                sound.Play();
+            }
+
+
+        }
+    }
+}
